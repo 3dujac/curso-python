5341694ac0af642a247bf4bd132374bfac0323a7
3duj.ac <eduardojosealonso@outlook.es>
2025-03-07 19:14:34 +0100

    repaso completo dia 1

--- 02_flow_control/repaso_clarse1.py
+++ 02_flow_control/repaso_clarse1.py
@@ -21,3 +21,19 @@ print(bool("")) #convierte a booleano y devuelve False. Cadena de texto vacia es
 
 print("esto convierte el numero 20 a cadena de texto: " + str(20))
 print("La suma de " + str(20) + " + " + str(30) + " es: " + str(20+30))
+
+#variables
+name="Eduardo"
+print("Mi nombre es: " + name)
+name=37 #cambio de tipo de variable mediante reasignacion
+anonacimiento=1982
+my_name="Eduardo"
+print("Mi edad es: " + str(name)) #tipado fuerte. No se puede concatenar cadena de texto con entero. No transforma automaticamente
+print(f"Mi edad es: {name} y mi año de nacicimiento es {anonacimiento}") #f-string. Forma mas facil de concatenar
+print("............................................")
+print(f"Hola {my_name}, tengo {name + 5} años y naci en {anonacimiento}")
+
+nombre = input("Cual es tu nombre? \n")
+print(f"Hola {nombre}") #input siempre devuelve una cadena de texto
+coutry, city = input("Cual es tu pais y ciudad? \n").split(",") #split separa los valores por coma
+print(f"Tu pais es {coutry} y tu ciudad es {city}")
