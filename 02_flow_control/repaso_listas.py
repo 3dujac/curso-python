c9bf8029ee1c13a387aebdcf245347d1b87cf7aa
3duj.ac <eduardojosealonso@outlook.es>
2025-03-10 22:21:58 +0100

    repaso listas 1

---
+++ 02_flow_control/repaso_listas.py
@@ -0,0 +1,29 @@
+import os
+os.system("cls")
+print("Repaso de listas")
+
+lista = ["uno", 2, 3.14, True]
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+
+print("-imprimir solo estas posiciones-")
+print(lista[0:1]) #el ultimo no se toma en cuenta
+print(lista[0:2])
+
+print(lista[:3]) #imprime desde el inicio hasta la posición 3, sin incluirla
+
+listac = [[1,2], ["a", "b"]]
+print(listac[0][0])
+print(listac[0][1])
+print(listac[1][0])
+print(listac[1][1])
+
+lista2 = [1, 2, 3, 4, 5]
+print("-imprimir indicado el paso-")
+print(lista2[::-1])
+
+print("-aquí va el for-")
+for i in lista2:
+    print(i)
